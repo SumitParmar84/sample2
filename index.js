0c15c259f886711bc7a05b30a13403ff8a173d68
SumitParmar84 <sumit.parmar84@gmail.com>
2023-06-13 22:53:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("Hello everyone")
+let name="Sumit"
+console.log(name)
+let p = "sumit"
+let q="raj"
+console.log(p);
+console.log("hello",p);
+console.log("how r u",q);
+console.log(q);
+let a=30,b=20;
+let c=a+b;
+let d=a-b;
+console.log ("Answer",c);
+console.log("Subtraction",d);
+//String
+/*let p = "sumit"
+let q="raj"
+console.log(p);
+console.log("hello",p);
+console.log("how r u",q);
+console.log(q);
+//Numbers
+let a=30,b=20;
+let c=a+b;
+let d=a-b;
+console.log ("Answer",c);
+console.log("Subtraction",d);
+console.log("Addition :",c+d);
+console.log("Multiplication:",c*d);*/
+a=10;
+if(a%2==0)
+{
+  console.log("Even Number");
+}
+else
+{
+  console.log("Odd Number");
+}
+
+//Single character
+let e='s';
+console.log("single Charaster",e);
